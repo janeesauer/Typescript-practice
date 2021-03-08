c5cc636d19da2e0fcfb3335e7b8c90285960105f
janeesauer <55198243+janeesauer@users.noreply.github.com>
2021-03-08 20:51:50 +0000

    Solid and narrowing

--- narrowing/narrowing.ts
+++ narrowing/narrowing.ts
@@ -2,3 +2,48 @@
 // https://2ality.com/2020/06/type-guards-assertion-functions-typescript.html
 // https://www.carlrippon.com/6-ways-to-narrow-types-in-typescript/
 // https://medium.com/@jackhmwilliams/type-narrowing-in-typescript-44a6757c5a64
+
+// Type guards using typeof - using strict equality (===)  
+// in type guard
+// type guard function with type predicate
+
+
+
+// narrowing via if
+
+
+// narrowing via switch
+
+
+// conditional value check
+
+
+// Truthiness 
+
+
+// guarding against null or undefined
+
+
+// Equality narrowing
+
+
+// instanceof
+// Array .isArray
+
+
+// assignments
+
+
+// control flow analysis
+
+
+// type predicates
+
+
+// Discriminated unions
+
+
+// the never type
+
+
+// Exhaustiveness checking
--- solid/solid.ts
+++ solid/solid.ts
@@ -10,6 +10,71 @@
 // and extend. They also make it easy for developers to avoid code smells, 
 // easily refactor code, and are also a part of the agile or adaptive software development.
 
-// https://samueleresca.net/solid-principles-using-typescript/
+
 // https://wanago.io/2020/02/03/applying-solid-principles-to-your-typescript-code/
-// https://itnext.io/brutally-solid-typescript-ba745585f440
+
+// S - Single responsibility
+
+
+// The following code is extracted from a part of the microfront end component i am currently working on.
+// As part of a micro front end a section of the page contains a tab section that has three tabs in a
+// tabPannel - Underneath the tabPannel the details of the selected tab is output to the screen.
+// The component is only doing one function.  It could be modified to add more pannels
+// and will most likely be changed to populate the tabs from a different source in the future.
+// 
+import React, { useState } from 'react'
+import { Tab } from './Tab';
+import { TabPanel } from './TabPanel';
+import './Tabs.scss';
+const tabs = ["Vehicle", "Driver", "Cover"];
+export const Tabs = () => {
+  const [selectedTab, setSelectedTab] = useState(0);
+  const tabPanels = [
+    {
+      index: 0,
+      component:
+        <TabPanel key="vehicle">
+          Vehicle Panel
+        </TabPanel>,
+    },
+    {
+      index: 1,
+      component:
+        <TabPanel key="driver">
+          Driver Panel
+        </TabPanel>,
+    },
+    {
+      index: 2,
+      component:
+        <TabPanel key="cover">
+          Cover Panel
+        </TabPanel>
+    }
+  ];
+  return (
+    <div className="tabs-wrapper">
+      <div className="tab-bar">
+        {tabs.map((tab, index) =>
+          <Tab
+            text={tab}
+            key={tab}
+            id={tab}
+            index={index}
+            handleClick={setSelectedTab}
+          />
+        )}
+      </div>
+      {tabPanels.map(panel => {
+        if (panel.index === selectedTab) return panel.component;
+        return null;
+      })}
+    </div>
+  )
+};
+
+// The Tab component displays the details of an Individual tab.  The component will be modified to 
+// to output a different layout. No additional functionality can be added for the component to 
+// fit into the single responsibility principle.
+//
+// 
